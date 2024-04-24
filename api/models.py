f2d61656175d0b4e3f2579fe93bcb2fc88463f21
Jehoshaphat Obol <86023492+Jehoshaphat-Obol@users.noreply.github.com>
2024-04-24 11:34:25 +0300

    improve guest serializer for better guest creation

--- api/models.py
+++ api/models.py
@@ -14,7 +14,7 @@ class Row(models.Model):
 
     def __str__(self):
         return f"Row {self.pk} in {self.cluster}"
-    
+
 class Seat(models.Model):
     row =models.ForeignKey(Row, on_delete=models.CASCADE)
     seat_number = models.PositiveIntegerField()
@@ -26,26 +26,40 @@ class Seat(models.Model):
         unique_together = ['row', 'seat_number'] # the same seat number can't re-occur on the same row
 
 class StudentProfile(models.Model):
+    class Status(models.TextChoices):
+        EXPECTED = 'EX', 'Expected'
+        POSTPONED = 'PP', 'Postponed'
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    graduation_status = models.BooleanField(default=False)
+    graduation_status = models.CharField(max_length=2, default=Status.EXPECTED, choices=Status.choices)
     degree_program = models.CharField(max_length=100)
     degree_level = models.CharField(max_length=100)
     college = models.CharField(max_length=100)
+    
     # Add more fields as needed
     
     def __str__(self):
         return self.user.username
 
 class Guest(models.Model):
+    class Type(models.TextChoices):
+        PARENT = 'PRT', 'Parent'
+        VIP = 'VIP', 'VIP'
+
+    class Status(models.TextChoices):
+        EXPECTED = 'EX', 'Expected'
+        POSTPONED = 'PP', 'Postponed'
     student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
+    type = models.CharField(max_length=3, default=Type.PARENT, choices=Type.choices)
+    status = models.CharField(max_length=2, default=Status.EXPECTED, choices=Status.choices)
+
     # Add more fields as needed
-    
+
     def __str__(self):
         return self.name
 
 class SeatAssignment(models.Model):
-    student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE)
+    student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE, default=None)
     seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
     # Add more fields like seat number if needed
     
--- api/serializers.py
+++ api/serializers.py
@@ -1,20 +1,26 @@
 from rest_framework import serializers
 from .models import Cluster, Row, StudentProfile, Guest, Seat, SeatAssignment
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.hashers import make_password
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["username", "email", "first_name", "last_name"]
+        fields = ["username", "password", "email", "first_name", "last_name"]
+        extra_kwargs = {
+            "password": {"write_only": True},  # Make password write-only
+        }
+    def create(Self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
 
 
 class ClusterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cluster
         fields = ["id", "name"]
-        
-
 
 
 class RowSerializer(serializers.ModelSerializer):
@@ -31,8 +37,7 @@ class SeatSerializer(serializers.ModelSerializer):
 
 
 class StudentProfileSerializer(serializers.ModelSerializer):
-    user = UserSerializer()  # Nesting User serializer
-
+    user = UserSerializer()
     class Meta:
         model = StudentProfile
         fields = [
@@ -43,12 +48,52 @@ class StudentProfileSerializer(serializers.ModelSerializer):
             "degree_level",
             "college",
         ]
+    
+    def create(self, validated_data):
+        user_data = validated_data.pop("user")
+        password = None
+        for field, value in user_data.items():
+            if(field == 'password'):
+                password = value
+        user_serializer = UserSerializer(data=user_data)
+        if user_serializer.is_valid():
+            if password:
+                # user_data["password"] = make_password(password)
+                pass# Hash the password
+            else:
+                # Handle the case where password is not provided
+                raise serializers.ValidationError("Password is required")
+
+            user = user_serializer.save()
+            student_profile = StudentProfile.objects.create(user=user, **validated_data)
+            return student_profile
+        else:
+            raise serializers.ValidationError(user_serializer.errors)
 
 
 class GuestSerializer(serializers.ModelSerializer):
+    student = serializers.CharField(source="student.user.username")
+
     class Meta:
         model = Guest
-        fields = ["id", "student", "name"]
+        fields = ["id", "student", "name", "type", 'status']
+        
+    def create(self, validated_data):
+        student_username = validated_data.pop('student')
+        
+        try:
+            # Attempt to get the student profile from the database
+            student_profile = StudentProfile.objects.get(user__username=student_username['user']['username'])
+        except ObjectDoesNotExist:
+            # Handle case where the student profile does not exist
+            raise serializers.ValidationError("Student profile does not exist.")
+
+        # Create the guest instance with the retrieved student profile and other validated data
+        guest = Guest(student=student_profile, **validated_data)
+        guest.save()
+        return guest
+        
+        
 
 
 class SeatAssignmentSerializer(serializers.ModelSerializer):
